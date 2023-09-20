da0ca6b8488ad233ecc6a200bfdeacaf5ccd180f
AlineJo <albaash247>
2023-09-20 11:48:00 +0400

    #relocate

--- pages/index.php
+++ pages/index.php
@@ -918,11 +918,11 @@
     <!-- modal-edit-task -->
 
 
-    <script src="../controllers/index.js"></script>
-    <script src="../controllers/login-form.js"></script>
-    <script type="module" src="../controllers/login-modal.js"></script>
-    <script type="module" src="../controllers/add-task-modal.js"></script>
-    <script type="module" src="../controllers/edit-task-modal.js"></script>
+    <script src="../js/controllers/index.js"></script>
+    <script src="../js/controllers/login-form.js"></script>
+    <script type="module" src="../js/controllers/login-modal.js"></script>
+    <script type="module" src="../js/controllers/add-task-modal.js"></script>
+    <script type="module" src="../js/controllers/edit-task-modal.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
